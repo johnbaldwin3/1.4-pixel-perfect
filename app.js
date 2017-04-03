630bb09afc02bbfb461c7661c4d7d9604a153328
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-04-03 11:15:06 -0400

    fixed murrays alignment at bottom

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// used knowledge from Team Treehouse, looked up idea in google - saw this:
+
 //http://stackoverflow.com/questions/6910049/on-a-css-hover-event-can-i-change-another-divs-styling
-//because I couldn't figure how to link these two events without a function to help...
+
 $(function() {
   $('.read-comments').hover(function() {
     // console.log("selected");
